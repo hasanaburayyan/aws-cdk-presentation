22543f859bdcd9fbc41a44edfbc923a754ea8e69
Hasan S. Abu-Rayyan <Hasanaburayyan21@gmail.com>
2021-09-16 10:49:30 -0400

    added buildspec changes

--- src/demo/my-first-stack.ts
+++ src/demo/my-first-stack.ts
@@ -1,5 +1,6 @@
 import * as codebuild from '@aws-cdk/aws-codebuild';
 import * as codecommit from '@aws-cdk/aws-codecommit';
+import * as s3 from '@aws-cdk/aws-s3';
 import * as iam from '@aws-cdk/aws-iam';
 import * as cdk from '@aws-cdk/core';
 
@@ -10,6 +11,11 @@ export class MyFirstStack extends cdk.Stack {
     const myRepo = new codecommit.Repository(this, 'my-code-repo', {
       repositoryName: 'cdk-demo',
       description: 'dont fail me now demo!',
+    });
+
+    const myBucket = new s3.Bucket(this, 'my-bucket', {
+      bucketName: 'hsa29-cool-demo-bucket',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
     new codebuild.Project(this, 'my-code-build-project', {
@@ -23,6 +29,12 @@ export class MyFirstStack extends cdk.Stack {
       }),
       environment: {
         buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+        environmentVariables: {
+          BUCKET_NAME: {
+            value: myBucket.bucketName,
+            type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+          },
+        },
       },
     });
   }
